9a6e4d4d07dc9e8b31792fa8a580bae8214e9936
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2024-11-30 01:42:29 +0530

    Display Mode component with dropdown Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import { useState } from "react";
 import './App.css'
+import DisplayMode from './components/DisplayMode/DisplayMode'
 
 function App() {
+  const [displayTheme, setDisplayTheme]=useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
  
 
-  return 
+  return (
+    <DisplayMode {...{displayTheme, setDisplayTheme, }}></DisplayMode>
+  )
 }
 
 export default App
---
+++ src/components/DisplayMode/DisplayMode.jsx
@@ -0,0 +1,26 @@
+import './DisplayMode.css'
+//import Dropdown from 'react-bootstrap/Dropdown';
+
+export default function DisplayMode({displayTheme, setDisplayTheme}){
+    return(
+        <>
+            <div className="dropdown themeDiv">
+                <img className="themeIcon dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false" src={displayTheme ? 'icons8-dark-mode-50.png' : 'icons8-daylight.svg'} alt="" />
+
+                <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" onClick={()=>{
+                        setDisplayTheme(false)
+                        }}>Light Mode</a></li>
+                    <li><a className="dropdown-item" onClick={()=>{
+                        setDisplayTheme(true)
+                        }}>Dark Mode</a></li>
+                    <li><a className="dropdown-item" onClick={()=>{
+                        console.log('sys');
+                        setDisplayTheme(window.matchMedia('(prefers-color-scheme: dark)').matches)
+                        }}>System Default</a></li>
+                </ul>
+            </div>
+
+        </>
+    )
+}
