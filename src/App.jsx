40774bb31ebc39b0690a4c01eda2d946bcd5ddda
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2024-12-03 14:21:10 +0530

    Created Search Button with Modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { useState } from "react";
 import './App.css'
 import DisplayMode from './components/DisplayMode/DisplayMode'
+import SearchButton from './components/Search Box/SearchButton';
 
 function App() {
   const [displayTheme, setDisplayTheme]=useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
  
 
   return (
-    <DisplayMode {...{displayTheme, setDisplayTheme, }}></DisplayMode>
+    <>
+    <DisplayMode {...{displayTheme, setDisplayTheme, }} />
+    <SearchButton {...{displayTheme}} />
+    </>
   )
 }
 
---
+++ src/components/Search Box/SearchButton.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import '../DisplayMode/DisplayMode.css'
+import './SearchButton.css'
+import fetchSubredditInfo from "./fetchSubredditInfo"
+
+export default function SearchButton({displayTheme}){
+
+    const [searchItem, setSearchItem]= useState('')
+
+    return(
+        <>
+            <button type="button" className={`btn search-button ${displayTheme ? 'dark':'light'}`} data-bs-toggle="modal" data-bs-target="#searchModal">
+                Search
+            </button>
+
+
+            <div className="modal fade" id="searchModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content">
+                <div className="modal-header">
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div className="modal-body row d-flex align-items-center justify-content-center">
+                    <div className="search-bar row col-11 d-flex align-items-center">
+                        <div className="search-icon col-auto">
+                            <img className="search-icon-img" src="icons8-search.svg" alt="bar" />
+                        </div>
+                        <input className="search-input-box col" type="text" placeholder="Sub-Reddit" value={searchItem} onChange={(e)=>{
+                            setSearchItem(e.target.value);
+                            console.log(searchItem);
+                        }} onKeyDown={(e)=>{
+                            if (e.key==='Enter'){
+                                fetchSubredditInfo(searchItem);
+                                console.log('enter');
+                            }
+                        }} />
+                    </div>
+                    
+                </div>
+                
+                </div>
+            </div>
+            </div>
+        </>
+    )
+}
