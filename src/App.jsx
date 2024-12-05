d60f619e003937f81ad16c396f99ee5c69bad2e6
KunalVChoudhary <imkunalvchoudhary@gmail.com>
2024-12-05 13:18:20 +0530

    Created FetchedSubPosts Components that give jsx array of posts for rendering

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { useEffect, useState } from "react";
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
 import DisplayMode from './components/DisplayMode/DisplayMode'
 import SearchButton from './components/Search Box/SearchButton';
 import fetchSubredditInfo from './components/FetchingData/fetchSubredditInfo';
 import FetchedSubPosts from './components/FetchingData/FetchedSubPosts'
+
 
 function App() {
   //const [displayTheme, setDisplayTheme]=useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
@@ -24,13 +26,13 @@ function App() {
  
 
   return (
-    <>
+    <BrowserRouter>
     {/* <DisplayMode {...{displayTheme, setDisplayTheme, }} />
     <SearchButton {...{displayTheme}} /> */}
     {subResponse? (<FetchedSubPosts {...{fetchResponse:subResponse}} />) : (<p>Kunal</p>)}
     
     
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/components/FetchingData/FetchedSubPosts.jsx
+++ src/components/FetchingData/FetchedSubPosts.jsx
@@ -1,14 +1,58 @@
+import './FetchedSubPosts.css'
+
 export default function fetchedSubPosts({fetchResponse}){
+    
    return(
-    <div>
+    <div className='sub-posts'>
     {
         fetchResponse.map((post,index)=>{
-            return (<div key={index}>
-                    <p>{post['data']['title']}</p>
-                    <p>{post['data']['ups']}</p>
-                    <p>{post['data']['downs']}</p>
-                    <p>{post['data']['num_comments']}</p>
-                    </div>)
+            return (
+                    <div className="post-container" key={index}>
+                        <div className="post d-flex align-items-center row" onClick={()=>{
+                            window.open(post['data']['url'], '_blank');
+                            console.log(post['data']['url']);
+                        }}>
+                            <div className="post-interactions  col-3 ">
+                                <div className="post-upvote d-flex col-12">
+                                    <div className="upvote-icon">
+                                        <img className='post-icon' src="icons8-up-48.png" alt="" />
+                                        {/* <p className='post-text'>{post['data']['ups']}</p> */}
+                                    </div>
+                                    <div className="upvote-number">
+                                        <p className='post-text'>{post['data']['ups']}</p>
+                                    </div>
+                                </div>
+                                <div className="post-downvote d-flex col-12 ">
+                                    <div className="downvote-icon">
+                                        <img className='post-icon' src="icons8-down-48.png" alt="" />
+                                        {/* <p className='post-text'>{post['data']['downs']}</p> */}
+                                    </div>
+                                    <div className="downvote-number">
+                                        <p className='post-text'>{post['data']['downs']}</p>
+                                    </div>
+                                </div>
+                                <div className="post-comment d-flex col-12">
+                                    <div className="comment-icon">
+                                        <img className='post-icon' src="icons8-comments-48.png" alt="" />
+                                        {/* <p className='post-text'>{post['data']['num_comments']}</p> */}
+                                    </div>
+                                    <div className="comment-number">
+                                        <p className='post-text'>{post['data']['num_comments']}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="post-title col-9">
+                                <div className="post-title ">
+                                    <p className='post-title-text'>{post['data']['title']}</p>
+                                    {/* <p className='post-text'>Posted By {post['data']['author']}</p> */}
+                                </div>
+                                <div className="post-author ">
+                                    <p className='post-text'>Posted By {post['data']['author']}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )
         })
     }
     </div>
